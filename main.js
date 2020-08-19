a0e8febe394435c6f1ca00215a8d9757c22cee0f
sonny_developer <66192341+JuhyungSon@users.noreply.github.com>
2020-08-19 16:15:16 +1000

    js final

--- main.js
+++ main.js
@@ -13,6 +13,13 @@ document.addEventListener('scroll', () => {
     }
 });
 
+//toggle
+
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+});
+
 //handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
@@ -21,15 +28,8 @@ navbarMenu.addEventListener('click', (event) => {
     if (link == null) {
         return;
     }
-
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
-});
-
-//toggle
-
-const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
-navbarToggleBtn.addEventListener('click', () => {
-    navbarMenu.classList.toggle('open');
 });
 
 //handle click on  "contact me" button on home
